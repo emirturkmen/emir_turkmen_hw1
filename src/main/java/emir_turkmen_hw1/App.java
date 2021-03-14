f3a289b933cdf610a49ddfe95e5693170324d3c0
emirturkmen <e.turkmen@etu.edu.tr>
2021-03-14 20:52:27 +0300

    solving an error in app.java

--- src/main/java/emir_turkmen_hw1/App.java
+++ src/main/java/emir_turkmen_hw1/App.java
@@ -10,8 +10,8 @@ import static spark.Spark.post;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
